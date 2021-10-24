f0cadd93ca3af8071c2aad8932c2fe4724c86369
Rohit <rohitbelure128@gmail.com>
2021-10-24 13:37:32 +0530

    Flask application with dynamic route

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello"
+	return "Hello"
 
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>hello, %s!</h1>' % name
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
