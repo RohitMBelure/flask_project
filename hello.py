039e6927de0276367e1b3be2c84ce48a67dc15f3
Rohit <rohitbelure128@gmail.com>
2021-10-25 20:41:04 +0530

    GET/POST/PUT/DELETE API endpoints

--- hello.py
+++ hello.py
@@ -1,14 +1,51 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello"
+languages = [{'name':'javascript'}, {'name':'python'}, {'name':'Ruby'}]
 
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>hello, %s!</h1>' % name
+@app.route('/', methods=['GET'])
+def test():
+	return jsonify({'message':'It works!'})
 
-if __name__ == "__main__":
+@app.route('/lang', methods=['GET'])
+def returnAll():
+	return jsonify({'languages':'languages'})
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+	langs = [language for language in languages if language['name'] == name]
+	return jsonify({'language':langs[0]})
+
+@app.route('/lang', methods=['POST'])
+def addOne():
+	language = {'name':request.json['name']}
+	languages.append(language)
+	return jsonify({'languages':languages})
+
+@app.route('/lang/<string:name>', methods=['PUT'])
+def editOne(name):
+	langs = [language for language in languages if language['name'] == name]
+	langs[0]['name'] = rerquest.json['name']
+	return jsonify({'language':langs[0]})
+
+@app.route('/lang/<string:name>', methods=['DELETE'])
+def removeOne(name):
+	lang = [language for language in languages if language['name'] == name]
+	languages.remove(lang[0])
+	return jsonify({'languages':language})
+
+if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
