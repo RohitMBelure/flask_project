30b75ad97484a124dc06ca398dc3843713712c24
Rohit <rohitbelure128@gmail.com>
2021-10-28 13:11:27 +0530

    Rendering Templates

--- hello.py
+++ hello.py
@@ -1,43 +1,14 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def load_user(dev):
-	return 'dev is a nice guy!!'
-
-@app.route('/')
+@app.route('/index')
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return '<p>Your browser is %s</p>' %user_agent
+    return render_template('index.html')
 
-@app.route('/bad')
-def bad():
-	return '<h1>Bad Request</h1>', 400
-
-@app.route('/response')
-def response():
-	response = make_response('<h1>This document carries a cookie!</h1>')
-	response.set_cookie('answer', '42')
-	return response
-
-@app.route('/home')
-def home():
-	return redirect('http://127.0.0.1:5000/')
-
-@app.route('/user/<id>')
-def get_user(id):
-	user = load_user(id)
-	if not user:
-		abort(404)
-	return '<h1>Hello, %s!</h1>' % user
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-
-
-
-
-
-
+    app.run(debug=True)
+	
