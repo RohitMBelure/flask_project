39c218af03b03d021724f0060730dd5421cc9b8b
Rohit <rohitbelure128@gmail.com>
2021-11-06 19:30:35 +0530

    Jinja2 Control Structures

--- hello.py
+++ hello.py
@@ -1,14 +1,27 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-	return "Hello"
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>hello, %s!</h1>' % name
+	return render_template('ifCS.html', user=name)
 
-if __name__ == "__main__":
-	app.run(debug=True)
+@app.route('/number')
+def number():
+	number=15
+	return render_template('ifCS.html', y=number)
+	
+@app.route('/home')
+def home():
+	variable=[1, 10, 40, 54, 58, 89, 98, 143]
+	return render_template('forCS.html', variable=variable)
+
+@app.route('/base')
+def base():
+	list1=[125, 54, 487, 597, 998, 597, 451, 665, 71]
+	list2=['rohit', 'Ram', 'shital', 'sneha', 'kartik', 'aarohi']
+	list3=['ram', 457, 'True', 459, 257, 'False']
+	return render_template('base.html', name='Laxmi', numbers=list1, list2=list2, list3=list3)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+		
