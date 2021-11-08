f17304de1b9c650dc1722f7cc140ad0c46c3e3be
Rohit <rohitbelure128@gmail.com>
2021-11-08 17:43:01 +0530

    Custom Error Page

--- hello.py
+++ hello.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('ifCS.html', user=name)
-
-@app.route('/number')
-def number():
-	number=15
-	return render_template('ifCS.html', y=number)
-	
-@app.route('/home')
-def home():
-	variable=[1, 10, 40, 54, 58, 89, 98, 143]
-	return render_template('forCS.html', variable=variable)
-
-@app.route('/base')
-def base():
-	list1=[125, 54, 487, 597, 998, 597, 451, 665, 71]
-	list2=['rohit', 'Ram', 'shital', 'sneha', 'kartik', 'aarohi']
-	list3=['ram', 457, 'True', 459, 257, 'False']
-	return render_template('base.html', name='Laxmi', numbers=list1, list2=list2, list3=list3)
+	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-		
+	app.run(debug=True)
